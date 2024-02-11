d315d1e3dc347c08fb4d4bff69ea630490664d4c
TettehSolomon <tettehsolomon106@gmail.com>
2024-02-11 15:51:40 +0100

    updated the mobile view of the home page

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.hamburger-menu');
-    const navbarList = document.querySelector('#navbar ul');
-
-    menuToggle.addEventListener('click', function () {
-        navbarList.classList.toggle('active');
-    });
-});
